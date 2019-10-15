2d8098fff8c10280aed31248bc4e0dd0119f6b50
divyavijayan <divyavijayan103@gmail.com>
2019-10-15 11:10:53 -0700

    Committing the modsels migrations config folders for sequelize

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const express = require('express');
 const app = express();
 
 //Require the custom routes into api
-require('./routes')(app);
+//require('./routes')(app);
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.',
+  }));
 module.exports = app;
