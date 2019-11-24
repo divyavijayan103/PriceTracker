192296f5f135d43c579fbf9501d108cf090ffa38
divyavijayan <divyavijayan103@gmail.com>
2019-11-24 14:59:08 -0800

    changes post to put for add to watch list route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const searchController= require('../controllers').search;
 module.exports = function(app){
     app.post('/pricetracker/register', userController.create);
     app.post('/pricetracker/login', userController.login);
-    app.get('/pricetracker/search',searchController.search)
+    app.get('/pricetracker/search',searchController.search);
+    app.put('/pricetracker/addToWatchList',searchController.addToWatchList);
+
 }
